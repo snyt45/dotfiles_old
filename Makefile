f71cc0c93e12fbf2669c36f1120bde5c1e4bdf91
yuta.sano <snyt45@gmail.com>
2020-03-15 06:55:15 +0900

    .gitignore_globalの導入、hyper.jsのクロスプラットフォーム対応

--- Makefile
+++ Makefile
@@ -11,6 +11,10 @@ DOTFILES_FILES := $(filter-out $(DOTFILES_EXCEPT), $(DOTFILES_TARGET))
 # makeコマンド
 ###################################################
 
+# hyper.js生成
+hyper_js_generate:
+	bash config/hyper_js_generate.sh
+
 # 更新
 update:
 	git pull origin master
@@ -20,4 +24,6 @@ update:
 
 # デプロイ
 deploy:
+	hyper_js_generate # Run make hyper_js_generate
 	@$(foreach val, $(DOTFILES_FILES), ln -sfnv $(abspath $(val)) $(HOME)/$(val);)
+
---
+++ config/hyper_js_generate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+##############################################################################
+# 定数
+##############################################################################
+DOTPATH=~/.dotfiles;
+HYPER_JS=.hyper.js;
+HYPER_JS_PATH=config/hyper_js_preferences;
+WINDOWS_HYPER_JS=$HYPER_JS_PATH/.windows.js;
+OTHER_HYPER_JS=$HYPER_JS_PATH/.other.js;
+
+##############################################################################
+# hyper.jsのクロスプラットフォーム化
+##############################################################################
+if [ "$(uname)" == "Darwin" ]; then
+  echo Mac
+  ln -snfv "$DOTPATH/$OTHER_HYPER_JS" "$HOME/$HYPER_JS"
+elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+  echo Windows
+  ln -snfv "$DOTPATH/$WINDOWS_HYPER_JS=" "$HOME/$HYPER_JS"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  echo Linux
+  ln -snfv "$DOTPATH/$OTHER_HYPER_JS" "$HOME/$HYPER_JS"
+else
+  echo Unknown OS
+fi
